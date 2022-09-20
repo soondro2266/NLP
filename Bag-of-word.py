4b99929ae2790309f49848cef0efe950c7017920
soondro2266 <100667910+soondro2266@users.noreply.github.com>
2022-09-20 19:34:20 +0800

    0734

--- Bag-of-word.py
+++ Bag-of-word.py
@@ -1,13 +1,29 @@
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
 
-train_data = [
-    "i love the book",
-    "this is a great book",
-    "the fit is great",
-    "i love the shoes"
+class Category:
+    BOOKS = "BOOKS"
+    CLOTHING = "CLOTHING"
+    
+    
+x = ["i love the book","this is a great book","the fit is great","i love the shoes"]
+y = [Category.BOOKS, Category.BOOKS, Category.CLOTHING, Category.CLOTHING]
+
+vectorizer = CountVectorizer(binary=True) #build the vectorizer
+x_vectors = vectorizer.fit_transform(x) #use x to make bag
+
+clf_svm = svm.SVC(kernel='linear') #build a classifier
+clf_svm.fit(x_vectors, y)
+
+test_data = [
+    "i like the book",
+    "the shoes is great",
+    "i love the pants",
+    "the book is great",
+    "shoes are alright"
 ]
 
-vectorizer = CountVectorizer(binary=True)
-vectors = vectorizer.fit_transform(train_data)
-print(vectorizer.get_feature_names())
-print(vectors.toarray())
+for test in test_data:
+    vector = vectorizer.transform([test]) #transform test_data to vector
+    result = clf_svm.predict(vector) #use vector to predict result
+    print(result)
